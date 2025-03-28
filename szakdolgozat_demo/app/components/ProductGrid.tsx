0d79e3e66d46eb6e6747451b8bc6d5131cc22be0
Balog Sebastian Máté <sebimbalog@gmail.com>
2025-03-28 22:34:27 +0100

    Pluss section

--- szakdolgozat_demo/app/components/ProductGrid.tsx
+++ szakdolgozat_demo/app/components/ProductGrid.tsx
@@ -47,6 +47,20 @@ const ProductGrid = () => {
               </div>
           ))}
         </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-6">
+          {products_second.map((src, index) => (
+              <div
+                  key={index}
+                  className="p-2 bg-white shadow-md transition-all duration-500 transform hover:scale-110 hover:rotate-3 hover:shadow-2xl hover:bg-gradient-to-r hover:from-orange-500 hover:to-white hover:opacity-90 hover:translate-y-[-41px]"
+              >
+                <img
+                    src={src}
+                    alt="Product"
+                    className="w-full h-auto transition-transform duration-500 ease-in-out"
+                />
+              </div>
+          ))}
+        </div>
       </div>
   );
 };
