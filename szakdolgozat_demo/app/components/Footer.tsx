1fb98133f3d8b9f3cd8a1b9fb1274000969258bc
Balog Sebastian Máté <sebimbalog@gmail.com>
2025-03-28 21:40:40 +0100

    Addig the style to the product element style

--- szakdolgozat_demo/app/components/Footer.tsx
+++ szakdolgozat_demo/app/components/Footer.tsx
@@ -3,11 +3,17 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <footer style={{ userSelect: "none" }} className="w-full font-inter font-mono text-black text-center p-6 bg-white shadow-md">
-      <p className="text-md text-black">© 2025 LIVETHE-FIT. All rights reserved.</p>
-    </footer>
-  );
+    return (
+        <footer
+            className="w-full font-inter font-mono text-black text-center p-6 bg-white shadow-md"
+            style={{
+                userSelect: "none", // Lehetővé teszi a szöveg kijelölésének letiltását
+                pointerEvents: "none", // Elősegíti, hogy a footer ne reagáljon egérkattintásokra
+            }}
+        >
+            <p className="text-md text-black">© 2025 LIVETHE-FIT. All rights reserved.</p>
+        </footer>
+    );
 };
 
 export default Footer;
--- szakdolgozat_demo/app/components/ProductGrid.tsx
+++ szakdolgozat_demo/app/components/ProductGrid.tsx
@@ -17,25 +17,36 @@ const products_second = [
 
 const ProductGrid = () => {
   return (
-    <div>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-6">
-      {products.map((src, index) => (
-        <div key={index} className="p-2 bg-white shadow-md">
-          <img src={src} alt="Product" className="w-full h-auto" />
+      <div>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-6">
+          {products.map((src, index) => (
+              <div
+                  key={index}
+                  className="p-2 bg-white shadow-md transition-all duration-500 transform hover:scale-110 hover:rotate-3 hover:shadow-2xl hover:bg-gradient-to-r hover:from-orange-500 hover:to-white hover:opacity-110 hover:translate-y-[-41px]"
+              >
+                <img
+                    src={src}
+                    alt="Product"
+                    className="w-full h-auto transition-transform duration-500 ease-in-out"
+                />
+              </div>
+          ))}
         </div>
-      ))}
-    </div>
-    <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-6">
-      {products_second.map((src, index) => (
-        <div key={index} className="p-2 bg-white shadow-md">
-          <img src={src} alt="Product" className="w-full h-auto" />
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-6">
+          {products_second.map((src, index) => (
+              <div
+                  key={index}
+                  className="p-2 bg-white shadow-md transition-all duration-500 transform hover:scale-110 hover:rotate-3 hover:shadow-2xl hover:bg-gradient-to-r hover:from-orange-500 hover:to-white hover:opacity-90 hover:translate-y-[-41px]"
+              >
+                <img
+                    src={src}
+                    alt="Product"
+                    className="w-full h-auto transition-transform duration-500 ease-in-out"
+                />
+              </div>
+          ))}
         </div>
-      ))}
-    </div>
-
-    </div>
-    
-    
+      </div>
   );
 };
 
