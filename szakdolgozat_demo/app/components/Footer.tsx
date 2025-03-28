8b4f0fb69a4c7b1d9c87925fd18fd4f27e02c890
Balog Sebastian Máté <sebimbalog@gmail.com>
2025-03-28 20:58:04 +0100

    Kis modositas, hogy ne lehessen nem kijllölni a dolgokat

--- szakdolgozat_demo/app/components/Footer.tsx
+++ szakdolgozat_demo/app/components/Footer.tsx
@@ -1,10 +1,10 @@
-"use client";
+"use client"
 
 import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full font-inter font-mono text-black text-center p-6 bg-white shadow-md">
+    <footer style={{ userSelect: "none" }} className="w-full font-inter font-mono text-black text-center p-6 bg-white shadow-md">
       <p className="text-md text-black">© 2025 LIVETHE-FIT. All rights reserved.</p>
     </footer>
   );
