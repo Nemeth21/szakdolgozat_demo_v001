c040b017fa1bee2becaff34cf635d911840613c3
Balog Sebastian Máté <sebimbalog@gmail.com>
2025-04-14 11:12:47 +0200

    rebase-refactor : the footer.tsx from the components. Maybe I will add an extra footer design cause right now I'm concerned about this one.

--- szakdolgozat_demo/app/components/Footer.tsx
+++ szakdolgozat_demo/app/components/Footer.tsx
@@ -7,13 +7,12 @@ const Footer = () => {
         <footer
             className="w-full font-inter font-mono text-black text-center p-6 bg-white shadow-md"
             style={{
-                userSelect: "none", // Lehetővé teszi a szöveg kijelölésének letiltását
-                pointerEvents: "none", // Elősegíti, hogy a footer ne reagáljon egérkattintásokra
+                userSelect: "none", 
+                pointerEvents: "none", 
             }}
         >
             <p className="text-md text-black">© 2025 LIVETHE-FIT. All rights reserved.</p>
         </footer>
     );
 };
-
 export default Footer;
